a6b6010979a7f31be5ade7cdad9d70ca44199724
H.A <35451876+fleurdecerisier22@users.noreply.github.com>
2022-07-22 16:01:38 +0900

    ver3

--- apps/Test.py
+++ apps/Test.py
@@ -1,8 +1,6 @@
 from apps.__packages import load_data, lottie
 import streamlit as st
-
-
-import time
+import numpy as np
 
 
 def app(): 
@@ -11,5 +9,8 @@ def app():
 
     st.write("data_num : ", len(data['primaryid']))
     st.write("OK DataLoad")    
+
+
+    st.write("age sum ",np.sum(data["age_yr"][:]))
 
     
